a8b323fa776870b91ac92ecb778b8aeecf9f9570
MUHAMMADWAQAS00 <129178566+MUHAMMADWAQAS00@users.noreply.github.com>
2024-11-04 09:18:03 -0800

    Add files via upload

---
+++ grade distribution.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int marks;
+	cout<<"enter your marks :";
+	cin>> marks;
+	
+	if(marks > 90)
+	{
+		cout<<"your grade is A :";
+		
+	}else if (marks >=80 && marks <=90)
+	{
+		cout<<" youre grade is B :";
+	}else{cout<<"your grade is C:";
+	}
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
